6437f619b2e0a8dca1203fddc55df02cc0ff49c7
timarciogarcia <timarciogarcia@gmail.com>
2022-02-15 11:36:42 -0300

    Post Categoria

--- src/main/java/br/com/cast/avaliacao/controller/CategoriaController.java
+++ src/main/java/br/com/cast/avaliacao/controller/CategoriaController.java
@@ -1,15 +1,19 @@
 package br.com.cast.avaliacao.controller;
 
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.cast.avaliacao.dto.CategoriaDto;
 import br.com.cast.avaliacao.model.Categoria;
@@ -34,5 +38,13 @@ public class CategoriaController {
 		List<CategoriaDto> listdto = list.stream().map(obj -> new CategoriaDto(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listdto);
 	}
-
+	
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+		obj = categoriaService.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	
 }
